cc8c2a201fd212eaba7e5ea7e45ef2139e21739d
SACHIN BHARDWAJ <sachinbhardwaj737@gmail.com>
2025-09-03 18:14:24 -0700

    Initial project setup

---
+++ services/book-service/src/test/java/com/bookBazaar/book_service/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookBazaar.book_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
